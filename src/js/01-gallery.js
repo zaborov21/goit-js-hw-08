d1349e37c355bca3231fbe27089d1b9ef3fe878a
Andrii Zaborovskyi <zaborov21@gmail.com>
2023-08-31 19:58:01 +0200

    add library of vimeo/player and lodash.throttle

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+
+const makeMurkup = () => {
+
+    const murkup = galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`
+    }).join("");
+    return murkup;
+}
+galleryRef.insertAdjacentHTML('afterbegin', makeMurkup());
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'top',
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
 
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+console.log(throttle);
+console.log(Player);
