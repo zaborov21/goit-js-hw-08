3eab03d8718af93c82d0d8f5b7a21733d5be2c43
Andrii Zaborovskyi <zaborov21@gmail.com>
2023-09-08 21:47:56 +0200

    completed homwork vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,33 @@
 
-import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
-console.log(throttle);
-console.log(Player);
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+
+
+const player = new Vimeo.Player(iframe);
+
+
+const onPlay = function (timeupdate) {
+
+    const currentTime = timeupdate.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+    // console.log(currentTime);
+
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
